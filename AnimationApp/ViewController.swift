2d78ccdae6fb80eeba50bf1d672d70e517eab111
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-09-21 16:15:49 +0300

    Added animation for view

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -11,8 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
     
+    private var animationStarted = false
+    
     @IBAction func runCoreAnimation(_ sender: UIButton) {
         sender.pulsate()
+        
+        UIView.animate(
+            withDuration: 0.5, // время за которое будет проделан путь 40 поинтов влево
+            delay: 0, // задержка
+            // массив с параметрами анимаций
+            // в блоке замыканий этого параметра мы передаем тот объект, к которому хотим применить анимации
+            options: [.autoreverse, .repeat]) {
+                // задаем смещение для рамки аутлета вью по оси Х
+                if !self.animationStarted {
+                    self.coreAnimationView.frame.origin.x -= 40
+                    self.animationStarted.toggle()
+                }
+            }
     }
 
 }
